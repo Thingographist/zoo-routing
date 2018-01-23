a506174368b34c3924c38df3564780a9393fd862
khoda <elizavetaskull@gmail.com>
2018-01-23 07:51:29 +0700

    define profiles for zookeeper versions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zoo-routing "0.1.0-SNAPSHOT"
+(defproject zoo-routing "0.1.0"
   :description "compojure for zookeeper"
   :url "https://github.com/Thingographist/zoo-routing"
   :license {:name "Eclipse Public License"
@@ -10,17 +10,18 @@
                  [clout "2.1.2" :exclusions [instaparse org.clojure/clojure]]
                  [instaparse "1.4.8"]
 
-                 ;; ZOOKEEPER 3.4
-                 ;[org.apache.zookeeper/zookeeper "3.4.11"]
-                 ;[org.apache.curator/curator-client "4.0.0" :exclusions [org.apache.zookeeper/zookeeper]]
-
-                 ;; ZOOKEEPER 3.5
-                 [org.apache.curator/curator-client "4.0.0"]
-
                  ;; CURATOR
                  [org.apache.curator/curator-framework "4.0.0"]
                  [org.apache.curator/curator-recipes "4.0.0"]
                  [org.apache.curator/curator-test "4.0.0"]]
+
+
+  :profiles {:dev         {:dependencies [[org.apache.curator/curator-client "4.0.0"]]} ;; ZOOKEEPER 3.5
+             :repl        {:dependencies [[org.apache.zookeeper/zookeeper "3.4.11"] ;; ZOOKEEPER 3.4
+                                          [org.apache.curator/curator-client "4.0.0" :exclusions [org.apache.zookeeper/zookeeper]]]}
+             :zookeeper-4 {:dependencies [[org.apache.zookeeper/zookeeper "3.4.11"] ;; ZOOKEEPER 3.4
+                                          [org.apache.curator/curator-client "4.0.0" :exclusions [org.apache.zookeeper/zookeeper]]]}
+             :zookeeper-5 {:dependencies [[org.apache.curator/curator-client "4.0.0"]]}}
 
   :repl-options {:host "0.0.0.0"
                  :port 4001})
